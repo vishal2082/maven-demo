ba058661b77b9656d798b43406744ea105f9c4d5
VISHAL <vishal.kumar2@dxc.com>
2019-04-14 00:01:08 +0530

    commint for maven-demo

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,11 @@
+package com.codebind;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my app");
+
+	}
+
+}
